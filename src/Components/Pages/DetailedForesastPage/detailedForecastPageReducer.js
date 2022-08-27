a0ef0c0ac8f434bd31ea1f3270843f1d884822d6
NatalliaAnashkevich <nataliaanashkevich@coherentsolutions.com>
2022-08-28 00:23:45 +0200

    detailedForecastPage with Immer

--- src/Components/Pages/DetailedForesastPage/detailedForecastPageReducer.js
+++ src/Components/Pages/DetailedForesastPage/detailedForecastPageReducer.js
@@ -1,26 +1,24 @@
 import initialState from './detailedForecastPageInitialState';
 import { DETAILED_FORECAST_PAGE_ACTIONS } from './detailedForecastPageActions';
+import produce from 'immer';
 
 export const detailedForecastPageReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case DETAILED_FORECAST_PAGE_ACTIONS.USER_POSITION_DETAILED_WEATHER_REQUESTED:
-      return {
-        ...state,
-        loading: true,
-        error: null
-      };
+      return produce(state, draft => {
+        draft.loading = true;
+        draft.error = null;
+      })
     case DETAILED_FORECAST_PAGE_ACTIONS.USER_POSITION_DETAILED_WEATHER_RECEIVED:
-      return {
-        ...state,
-        loading: false,
-        hourlyWeather: payload.hourlyWeather
-      };
+      return produce(state, draft => {
+        draft.loading = false;
+        draft.hourlyWeather = payload.hourlyWeather;
+      });
     case DETAILED_FORECAST_PAGE_ACTIONS.USER_POSITION_DETAILED_WEATHER_FAILED:
-      return {
-        ...state,
-        loading: false,
-        error: payload
-      };
+      return produce(state, draft => {
+        draft.loading = false;
+        draft.error = payload;
+      });
     default:
       return state;
   }
