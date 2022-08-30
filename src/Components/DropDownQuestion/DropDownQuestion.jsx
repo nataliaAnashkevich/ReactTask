fa6f065a8dccb58ba0b25ca433dfc5cfab262e35
NatalliaAnashkevich <nataliaanashkevich@coherentsolutions.com>
2022-08-30 02:41:47 +0200

    Contact and About pages

---
+++ src/Components/DropDownQuestion/DropDownQuestion.jsx
@@ -0,0 +1,27 @@
+const region = [
+  'Not selected',
+  'Asia',
+  'Africa',
+  'Europe',
+  'North America',
+  'South America',
+  'Australia/Oceania'
+];
+
+function DropDownQuestion({ onChange, value }) {
+  const handleLocationChange = e => {
+    onChange(e.target.value);
+  };
+
+  return (
+    <>
+      <select value={value} onChange={handleLocationChange}>
+        {region.map(item => (
+          <option key={item}>{item}</option>
+        ))};
+      </select>
+    </>
+  );
+}
+
+export default DropDownQuestion;
--- src/Components/Pages/ContactPage/ContactPage.jsx
+++ src/Components/Pages/ContactPage/ContactPage.jsx
@@ -1,8 +1,58 @@
+import { useCallback, useState } from 'react';
+import RadioButtonQuestion from '../../RadioButtonQuestion/RadioButtonQuestion';
+import DropDownQuestion from '../../DropDownQuestion/DropDownQuestion';
+
 function ContactPage() {
+  const [frequencyAnswer, setFrequencyAnswer] = useState();
+  const [textareaValue, setTextareaValue] = useState();
+  const [regionValue, setRegionValue] = useState('Not selected');
+  const [isError, setIsError] = useState(false);
+
+  const handleOptionChange = useCallback(option => {
+    setFrequencyAnswer(option);
+  }, []);
+
+  const handleTextareaChange = useCallback(e => {
+    setTextareaValue(e.target.value);
+  }, []);
+
+  const handleLocationChange = useCallback(option => {
+    setRegionValue(option);
+  }, []);
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    if (!frequencyAnswer || !textareaValue) {
+      setIsError(true);
+    } else {
+      setIsError(false);
+      localStorage.setItem('SurveyResult', `${frequencyAnswer}, ${textareaValue}, ${regionValue}`);
+      setTextareaValue('');
+      setFrequencyAnswer(null);
+      setRegionValue('Not selected');
+    }
+  };
+
   return (
-    <div>
+    <>
       <h1>Contact</h1>
-    </div>
+      <form onSubmit={handleFormSubmit}>
+        <p>How often do you check weather forecast?*</p>
+        <RadioButtonQuestion onChange={handleOptionChange} value={frequencyAnswer} />
+
+        <p>What can we improve?*</p>
+        <textarea value={textareaValue} onChange={handleTextareaChange} />
+
+        <p>What is your region?:</p>
+        <DropDownQuestion onChange={handleLocationChange} value={regionValue} />
+
+        {isError && <p style={{ color: 'red' }}>Fill all required fields!</p>}
+        <br />
+        <div>
+          <button type="submit">Send feedback</button>
+        </div>
+      </form>
+    </>
   );
 }
 
